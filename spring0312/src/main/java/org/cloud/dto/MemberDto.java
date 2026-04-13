e779b60bc618e6bd0c3ec765460c4c1ff5727c7f
jaehyeon1119 <leajae@naver.com>
2026-04-13 15:39:27 +0900

    Sping0312

---
+++ spring0312/src/main/java/org/cloud/dto/MemberDto.java
@@ -0,0 +1,37 @@
+package org.cloud.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+
+	private String userId;
+	private String password;
+	private String userName;
+	private String role;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
