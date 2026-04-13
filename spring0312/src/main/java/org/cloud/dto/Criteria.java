ef21515c1faec278c1b95658e80b8fd828af354f
jaehyeon1119 <leajae@naver.com>
2026-04-13 16:45:39 +0900

    spring0312

---
+++ spring0312/src/main/java/org/cloud/dto/Criteria.java
@@ -0,0 +1,51 @@
+package org.cloud.dto;
+
+import lombok.Data;
+
+@Data
+public class Criteria {
+	private int pageNum;
+	private int amount;
+	private int skip;
+
+	public int getSkip() {
+		return skip;
+	}
+
+	public void setSkip(int skip) {
+		this.skip = skip;
+	}
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public Criteria() {
+		this(1, 10);
+
+	}
+
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum <= 0 ? 1 : pageNum;
+		this.amount = amount;
+		this.skip = (this.pageNum - 1) * amount;
+	}
+
+	public void setPageNum(int pageNum) {
+		if (pageNum <= 0) {
+			this.pageNum = 1;
+		} else {
+			this.pageNum = pageNum;
+		}
+		this.skip = (this.pageNum - 1) * amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+		this.skip = (this.pageNum - 1) * amount;
+	}
+}
